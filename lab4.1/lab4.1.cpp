add64335f218c709b8cfdb7ff54ee5ffdf25f102
X13X666X <newmicrogamer@gmail.com>
2024-11-06 00:36:00 +0200

    upload lab4.1

---
+++ lab4.1/lab4.1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int K, N, i;
+    double S;
+
+    cout << "K = "; cin >> K;
+    cout << "N = "; cin >> N;
+    
+    S = 1.0;
+    i = K;
+    while (i <= N) {
+        S *= ((K * 1.0 / i) + (i * 1.0 / N));
+        i++;
+    }
+    cout << "while: " << S << endl;
+
+    S = 1.0;
+    i = K;
+    do {
+        S *= ((K * 1.0 / i) + (i * 1.0 / N));
+        i++;
+    } while (i <= N);
+    cout << "do-while: " << S << endl;
+
+    S = 1.0;
+    for (i = K; i <= N; i++) {
+        S *= ((K * 1.0 / i) + (i * 1.0 / N));
+    }
+    cout << "for (++): " << S << endl;
+
+    S = 1.0;
+    for (i = N; i >= K; i--) {
+        S *= ((K * 1.0 / i) + (i * 1.0 / N));
+    }
+    cout << "for (--): " << S << endl;
+
+    return 0;
+}
